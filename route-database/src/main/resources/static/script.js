17c5b36e6730cb4d02007933f19ae8cc91ecfe1f
Christopher <c.millerscott@gmail.com>
2021-06-27 09:31:57 -0500

    functions created for confirms

--- route-database/src/main/resources/static/script.js
+++ route-database/src/main/resources/static/script.js
@@ -1,8 +1,18 @@
-function processConfirm(){
+function deleteAreaConfirm(){
     let editForm = document.getElementById("editForm");
 
-    let del = confirm("Removing this area will also remove the area's routes!\nAre you sure you want to proceed?");
+    let del = confirm("Removing this area will also remove this area's routes!\n\nAre you sure you want to proceed?");
         if(del == false) {
             editForm.onsubmit = event.preventDefault();
         }
     }
+
+
+function deleteRouteConfirm(){
+     let editForm = document.getElementById("editForm");
+
+     let del = confirm("Area you sure you want to remove this route?");
+         if(del == false) {
+             editForm.onsubmit = event.preventDefault();
+         }
+     }
